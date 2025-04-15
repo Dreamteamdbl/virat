cdd338ac0cba92bc87c3abb0e3872531817e042f
Dreamteamdbl <amazingyaan@gmail.com>
2025-04-15 21:57:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 7827463899))
+OWNER = int(os.environ.get("OWNER", 6133985472))
 
 #LOG = -1002159628443,
 
